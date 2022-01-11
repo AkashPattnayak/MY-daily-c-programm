4ffbbac0c5dd7ed62607676aee437c08b0499ee5
AkashPattnayak <pattnayakakash00@gmail.com>
2022-01-11 23:56:10 +0530

    11/jan 1st program

---
+++ Input-Output_Problems/Test-For-Format.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,x,y,z;
+    int p,q,r;
+    printf("Enter three integer numbers\n");
+    scanf("%d %*d %d",&a,&b,&c);
+    printf("%d %d %d\n\n",a,b,c);
+    printf("Enter two 4-digit number\n");
+    scanf("%2d %4d",&x,&y);
+    printf("%d %d\n\n",x,y);
+    printf("Enter two integer\n");
+    scanf("%d %d",&a,&b);
+    printf("%d %d\n\n",a,x);
+    printf("Enter a 9-digit number\n");
+    scanf("%3d %4d %3d",&p,&q,&r);
+    printf("%d %d %d\n\n",p,q,r);
+    printf("Enter two three digit number\n");
+    scanf("%d %d",&x,&y);
+    printf("%d %d",x,y);
+
+
+
+}
