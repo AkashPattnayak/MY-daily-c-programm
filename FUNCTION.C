cc7068f8754eb7dfad7d8a37135bccffd5dbb8c4
AkashPattnayak <pattnayakakash00@gmail.com>
2022-01-25 18:31:48 +0530

    factorial using function

---
+++ FUNCTION.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int fact(int);
+int main(){
+    int num,k;
+    printf("Enter a number :");
+    scanf("%d",&num);
+    k=fact(num);
+    printf("factorial = %d",k);
+   
+ return 0;
+
+}
+int fact(int n){
+     if(n>=1)
+  {
+      return n*fact(n-1);
+  }
+  else{
+     return 1;
+  }
+}
+ 
+
